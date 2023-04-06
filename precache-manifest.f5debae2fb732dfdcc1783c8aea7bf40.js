9b191413f8b3dcfe77ac81d19b1f0f29b3a8772c
Ari Ruotsalainen <ari.ruotsalainenn@gmail.com>
2023-04-06 13:45:05 +0300

    Updates

---
+++ precache-manifest.f5debae2fb732dfdcc1783c8aea7bf40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd3d8750127f4419c8de",
+    "url": "/Ariruo/pokemon-api/static/css/main.19647aba.chunk.css"
+  },
+  {
+    "revision": "fd3d8750127f4419c8de",
+    "url": "/Ariruo/pokemon-api/static/js/main.fd3d8750.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/Ariruo/pokemon-api/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "36713bb88ecb05c711ec",
+    "url": "/Ariruo/pokemon-api/static/js/runtime~main.36713bb8.js"
+  },
+  {
+    "revision": "d08047ebd4a7980eaf6bc056fca49889",
+    "url": "/Ariruo/pokemon-api/index.html"
+  }
+];
